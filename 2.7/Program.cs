78652a13f29325e346972f8c75473bbc5538a077
DoktorMandarin <faa1995alex@gmail.com>
2022-06-21 18:38:26 +0300

    Add project files.

---
+++ 2.7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frameWidht = "";
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите символ: ");
+            var simbol = Console.ReadLine();
+            var additionToLength = 4;
+            var emptySpaceForSecondLine = " ";
+            Console.WriteLine($"Символ: {simbol}");
+                       
+            for (var i = 1; i <= (name.Length + additionToLength); i += 1)
+            {
+                    frameWidht += simbol;
+            }
+
+            Console.WriteLine(frameWidht);
+            Console.WriteLine(frameWidht[0] + emptySpaceForSecondLine + name + emptySpaceForSecondLine + frameWidht[frameWidht.Length - 1]);
+            Console.WriteLine(frameWidht);
+        }
+    }
+}
